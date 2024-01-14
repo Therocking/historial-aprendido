aa896d0698ff8415309c95ca9e844705362fef32
jose <jgenao588@gmail.com>
2024-01-14 10:00:05 -0400

    14-01-2024 10:00:04

--- data-structure/tree/binary_tree/bt.ts
+++ data-structure/tree/binary_tree/bt.ts
@@ -1,39 +1,131 @@
 
-class BT {
-   private value: number;
-   public right: BT | null;
-   public left: BT | null;
+export class Node {
+   public value: number;
+   public right: Node | null;
+   public left: Node | null;
 
    constructor(val: number) {
       this.value = val;
-      this.right = null;
-      this.left = null;
+      this.left = this.right = null;
+   }
+}
+
+class BT {
+   public root: Node | null;
+
+   constructor() {
+      this.root = null;
    }
 
-   // Busqueda de un valor en un árbol
-   public exist(t: BT | null, val: number): boolean {
+   private searchRecursively(node: Node | null, val: number): boolean {
       // Si el árbol es nulo retorna false
-      if(t === null) return false;
+      if(node === null) return false;
       // Si la raiz es igual al valor que se busca retorna true
-      if(t.value === val) return true;
+      if(node.value === val) return true;
       // Si el valor que se busca es menor a la raiz, se de forma recursiva el valor a la izquierda
-      if(val < t.value) 
-	 return this.exist(t.left, val)
-      // De lo contrario, si el valor que se busca es menor a la raiz, se de forma recursiva el valor a la derecha
-      else return this.exist(t.right, val);
+      if(val < node.value) {
+	 return this.searchRecursively(node.left, val)
+      }
+      //De lo contrario, si el valor que se busca es menor a la raiz, se de forma recursiva el valor a la derecha
+      else {
+	 return this.searchRecursively(node.right, val)
+      }
+   }
+
+   private inserRecursively(node: Node | null, val: number): Node {
+      if(node === null ) {
+	 return new Node(val)
+      }
+
+      if(val < node.value) {
+	 node.left = this.inserRecursively(node.left, val)
+      }else if(val > node.value) {
+	 node.right = this.inserRecursively(node.right, val)
+      }
+
+      return node
+   }
+
+   private deleteRecursively(node: Node | null, val: number): Node | null {
+      // Encontrar el valor en el árbol
+      if(node === null) return node;
+
+      // Si el valor del nodo actual es igual al valor que se quiere eliminar retorna el valor
+      if(node.value === val) {
+	 node = null
+	 return node
+      }
+
+      if(val < node.value) {
+	 node.left = this.deleteRecursively(node.left, val);
+      }else if(val > node.value) {
+	 node.right = this.deleteRecursively(node.right, val);
+      }
+
+      return node
+   }
+
+   private displayInorder(node: Node | null): void {
+      // Si el node es nulo, no hacer nada
+      if(node === null) return;
+
+      console.log(node.value)
+      this.displayInorder(node.left)
+      this.displayInorder(node.right)
+   }
+
+   private limits(node: Node | null): void {
+      // Si el node es nulo, no hacer nada
+      if(node === null) return;
+
+      // Si el nodo es un hoja, la muestra por consola
+      if(node.left === null && node.right === null) {
+	 console.log("Node leaf:", node.value)
+      }
+
+      // Llamadas recursivas del nodo derecho e izquierdo
+      this.limits(node.right)
+      this.limits(node.left)
+   } 
+
+   /*Métodos*/
+
+   // Busqueda de un valor en un árbol
+   public exist(val: number): boolean {
+      return this.searchRecursively(this.root, val)
+   }
+   
+   // Insersión
+   public insert(val: number): void {
+      this.root = this.inserRecursively(this.root, val)
+   }
+
+   // Eliminación
+   public delete(val: number): void {
+      this.root = this.deleteRecursively(this.root, val)
    }
 
    //
-   /*public insert(t: BT | null, val: number) {
-      if(t === null) {
-	 const temp = new BT(val);
-	 t = temp
-	 return t
-      } else if(val < t.value) {
-	 this.insert(t.left, val)
-      }else this.insert(t.right, val)
-   }*/
+   public inorder(): void {
+      this.displayInorder(this.root)
+   }
+
+   // Muestras las hojas
+   public showLeaves(): void {
+      this.limits(this.root)
+   }
 }
 
-const root = new BT(5);
-console.log("Operación de búsqueda", root.exist(root, 20))
+const bt = new BT()
+bt.insert(25)
+bt.insert(33)
+bt.insert(17)
+bt.insert(45)
+bt.insert(0)
+bt.insert(1)
+bt.insert(100)
+bt.showLeaves()
+//bt.inorder()
+//bt.delete(20)
+//console.log("Element is in the tree:", bt.exist(33))
+//console.log(bt)
